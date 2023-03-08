b64e23ac6bf7063d70cf336941648dfd4d04cb08
rogers <variablecode404@gmail.com>
2023-03-09 01:37:37 +0300

    first commit

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 # ldc2 -release -O --Oz --O5 --DRT-gcopt=profile:2\
 # ldc2\
 # ldc2 -release -O --Oz --O5 --DRT-gcopt=profile:2 -vgc\
-dmd -release -O\
+dmd -release -inline -O\
 	src/esau.d\
 	\
 	\
@@ -45,3 +45,5 @@ dmd -release -O\
 
 rm esau.o
 # ./esau main.eu
+
+# C:\ldc2\ldc2-1.30.0-windows-x86\bin\ldc2.exe -release -O --Oz -of=esau.exe src/esau.d src/ast/node.d src/ast/exec.d src/ast/lexer.d src/ast/inter.d src/ast/parser.d src/ast/bytecode.d src/ast/bytecode2.d src/objects/function.d src/objects/object.d src/objects/type.d src/modules/importlib.d src/modules/base64.d src/modules/bytes.d src/modules/console.d src/modules/dict.d src/modules/dtypes.d src/modules/file.d src/modules/flare.d src/modules/_json.d src/modules/list.d src/modules/math.d src/modules/path.d src/modules/random.d src/modules/socket.d src/modules/string.d src/modules/sys.d src/modules/time.d
